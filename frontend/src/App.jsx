b231261eb64c0780285888f80f114313156dbaf0
iamrossan <roodhikari@gmail.com>
2025-06-27 17:16:45 -0400

    All the setting and message UI is updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,32 @@
 import { Route, Routes, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useAuthStore } from "./store/authStore";
+import Loader from "./components/Loader";
+import DashboardLayout from "./layouts/DashboardLayout";
+
+import DashboardPage from "./pages/DashboardPage";
+import NotificationPage from "./pages/DashboardPages/NotificationPage";
+import MessagePage from "./pages/DashboardPages/MessagePage";
+import SettingPage from "./pages/DashboardPages/SettingPage";
+import UploadPage from "./pages/DashboardPages/UploadPage";
+import FindPage from "./pages/DashboardPages/FindPage";
+import ProfilePage from "./pages/DashboardPages/ProfilePage";
 import SignUpPage from "./pages/SignUpPage";
 import LogInPage from "./pages/LogInPage";
 import EmailVerification from "./pages/EmailVerification";
 import ForgotPassword from "./pages/ForgotPassword";
-import { useEffect } from "react";
-import { useAuthStore } from "./store/authStore";
-import DashboardPage from "./pages/DashboardPage";
-import Loader from "./components/Loader";
 import ResetPasswordPage from "./pages/ResetPasswordPage";
 
-// protect routes that require authentication
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, user } = useAuthStore();
-  if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
-  }
-  if (!user.isVerified) {
-    return <Navigate to="/verifyEmail" replace />;
-  }
+  if (!isAuthenticated) return <Navigate to="/login" replace />;
+  if (!user.isVerified) return <Navigate to="/verifyEmail" replace />;
   return children;
 };
 
-//redirect user to the home page if they are already authenticated
 const RedirectAuthenticatedUser = ({ children }) => {
   const { isAuthenticated, user } = useAuthStore();
-  if (isAuthenticated && user.isVerified) {
-    return <Navigate to="/" replace />;
-  }
+  if (isAuthenticated && user.isVerified) return <Navigate to="/" replace />;
   return children;
 };
 
@@ -42,55 +42,66 @@ function App() {
         <Loader size={40} />
       </div>
     );
+
   return (
-    <>
-      {/* bg-gradient-to-r from-slate-500 to-slate-800 */}
-      <div>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <DashboardPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/signup"
-            element={
-              <RedirectAuthenticatedUser>
-                <SignUpPage />
-              </RedirectAuthenticatedUser>
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <RedirectAuthenticatedUser>
-                <LogInPage />
-              </RedirectAuthenticatedUser>
-            }
-          />
-          <Route path="/verifyEmail" element={<EmailVerification />} />
-          <Route
-            path="/forgotPassword"
-            element={
-              <RedirectAuthenticatedUser>
-                <ForgotPassword />
-              </RedirectAuthenticatedUser>
-            }
-          />
-          <Route
-            path="/reset-password/:token"
-            element={
-              <RedirectAuthenticatedUser>
-                <ResetPasswordPage />
-              </RedirectAuthenticatedUser>
-            }
-          ></Route>
-        </Routes>
-      </div>
-    </>
+    <Routes>
+      <Route
+        element={
+          <ProtectedRoute>
+            <DashboardLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/messages" element={<MessagePage />} />
+        <Route path="/settings" element={<SettingPage />} />
+        <Route path="/upload" element={<UploadPage />} />
+        <Route path="/find" element={<FindPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Route>
+
+      <Route
+        path="/signup"
+        element={
+          <RedirectAuthenticatedUser>
+            <SignUpPage />
+          </RedirectAuthenticatedUser>
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          <RedirectAuthenticatedUser>
+            <LogInPage />
+          </RedirectAuthenticatedUser>
+        }
+      />
+      <Route
+        path="/verifyEmail"
+        element={
+          <RedirectAuthenticatedUser>
+            <EmailVerification />
+          </RedirectAuthenticatedUser>
+        }
+      />
+      <Route
+        path="/forgotPassword"
+        element={
+          <RedirectAuthenticatedUser>
+            <ForgotPassword />
+          </RedirectAuthenticatedUser>
+        }
+      />
+      <Route
+        path="/reset-password/:token"
+        element={
+          <RedirectAuthenticatedUser>
+            <ResetPasswordPage />
+          </RedirectAuthenticatedUser>
+        }
+      />
+    </Routes>
   );
 }
 
