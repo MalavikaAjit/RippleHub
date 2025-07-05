151bc53c6105e4969452ba0148cf008f01f2c342
AcharyaAanish <acharyaanish16@gmail.com>
2025-07-04 22:30:11 -0400

    Implementing the schema for and multer for the post

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,16 +6,25 @@ import { connectDB } from "./db/connectDB.js";
 
 import authRoutes from "./routes/auth.route.js";
 
+import postRoutes from "./routes/post.route.js";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 2057;
 app.use(cors({ origin: "http://localhost:5173", credentials: true })); //allows us to make requests from the frontend to the backend
 
-app.use(express.json()); //allows us to parse incoming requests:req.body
+app.use(express.json()); //allows us to parse incoming requests:req.body 
+
+app.use('/uploads', express.static('uploads/'));
 
 app.use(cookieParser()); //allows us to parse cookies from incoming requests:req.cookies
 
 app.use("/api/auth", authRoutes); //routes for api auth
+
+app.use("/api", postRoutes);
+
+
+
 
 app.listen(PORT, () => {
   connectDB();
